23b62d0c41c5dbef899cfe82b30cdae500d9878c
Rabia Akhtar <rabia1082@gmail.com>
2015-02-26 08:41:43 -0500

    merge

--- hw4-mergesort/MergeSort.java
+++ hw4-mergesort/MergeSort.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class MergeSort {
+    private ArrayList<Integer> l;
 
-    public MergeSort(){
-       	ArrayList<Integer> l = new ArrayList<Integer>(7);
+    public MergeSort(ArrayList<Integer> l){
 	Random r =  new Random();
 	for (int i = 0; i<l.size();i++){
 	    int x = r.nextInt(100);
@@ -12,16 +12,16 @@ public class MergeSort {
 	}
     }
     public String toString (){
-	String s;
+	String s="";
 	for (int y =0; y<l.size();y++){
 	    s+=l.get(y);
 	}
 	return s;
     }
     public static void main (String[] args){
-
-	MergeSort(l);
-	System.out.println(MergeSort.toString());
+	ArrayList<Integer> l = new ArrayList<Integer>(7);
+	MergeSort m = new MergeSort(l);
+	System.out.println(m);
 
     }
 }
