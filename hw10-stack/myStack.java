dc02fd2c8829099d56a3290c5cc3cdd59bc7fea8
Rabia Akhtar <rabia1082@gmail.com>
2015-03-23 08:37:15 -0400

    hw

---
+++ hw10-stack/myStack.java
@@ -0,0 +1,22 @@
+public class myStack {
+
+    public myStack<E> () {
+
+    }
+
+    public boolean isEmpty (){
+
+    }
+
+    public E peek(){
+    }
+
+    public void push (){
+    }
+    public E pop () {
+    }
+
+    public static void main (String[] args) {
+    }
+
+}
