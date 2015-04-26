53bec7373a40a5dd02c6a4b9762428e1fc2ec0b7
rabia-akhtar <rabia1082@gmail.com>
2015-04-26 19:04:36 -0400

    hw

--- hw14-bst/Tree.java
+++ hw14-bst/Tree.java
@@ -9,7 +9,23 @@ public class Tree {
 	r = n;
     }
     public void insert (Node t, int i){
-
+	Node in = new Node(i);
+	if (t == null)
+	    t = in;
+	Node tmp = t;
+	Node x = null;
+	while (tmp != null){
+	    x  = tmp;
+	    int c = t.getData().compareTo(i);
+	    if (c < 0)
+		tmp = tmp.getRight();
+	    else if (c > 0)
+		tmp = tmp.getLeft();
+	}
+	if (i > x.getData())
+	    x.setRight(in);
+	else if (i < x.getData())
+	    x.setLeft(in);
     }
 
     public Node search (Node t, int i) {
